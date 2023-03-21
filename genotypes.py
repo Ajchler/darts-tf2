60c740c0e2a8d0c00813461ef75b54e7610e9ee2
Vojtech Eichler <vojta.eich@gmail.com>
2023-03-22 00:00:33 +0100

    Start experimenting with coatnet-esque operations

--- genotypes.py
+++ genotypes.py
@@ -3,22 +3,23 @@ import tensorflow as tf
 
 Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
 
-PRIMITIVES = [
-    'none',
-    'avg_pool_3x3',
-    'max_pool_3x3',
-    'skip_connect',
-    'sep_conv_3x3',
-    'sep_conv_5x5',
-    'dil_conv_3x3',
-    'dil_conv_5x5'
-]
+#PRIMITIVES = [
+#    'none',
+#    'avg_pool_3x3',
+#    'max_pool_3x3',
+#    'skip_connect',
+#    'sep_conv_3x3',
+#    'sep_conv_5x5',
+#    'dil_conv_3x3',
+#    'dil_conv_5x5'
+#]
 
-COATNET_PRIMITIVES = [
+PRIMITIVES = [
     'conv_3x3',
     'dconv_3x3',
     'conv_1x1',
     'rel_attention',
     'ffn',
-    'none'
+    'none',
+    'skip_connect'
 ]
