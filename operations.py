ef96ce10c6e6afed2453a41e4ac9ca79dd3f433b
Vojtech Eichler <vojta.eich@gmail.com>
2023-03-24 15:07:52 +0100

    Implement layer similar to dilated separable convolution

--- operations.py
+++ operations.py
@@ -34,12 +34,14 @@ class DilConv(keras.layers.Layer):
         #if approx:
         #    self.sep_conv = FakeApproxConv2D(C_curr, kernel_size, stride, 'same')
         #else:
-        self.sep_conv = keras.layers.SeparableConv2D(C_curr, kernel_size, stride, dilation_rate=rate, padding='same')
+        self.dw = keras.layers.DepthwiseConv2D(kernel_size, (1, 1), dilation_rate=rate, padding='same')
+        self.pw = keras.layers.Conv2D(C_curr, 1, stride, padding='same')
         self.bn = keras.layers.BatchNormalization()
 
     def call(self, x, training=None):
         x = self.relu(x)
-        x = self.sep_conv(x)
+        x = self.dw(x)
+        x = self.pw(x)
         x = self.bn(x)
         return x
 
